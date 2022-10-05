9f8506d05a43b885724a24320681381897a38e0a
Nas Samoyloff <samo0018@algonquinlive.com>
2022-10-04 23:10:18 -0400

    final

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
+// targeting the H1 
 const Title = document.getElementById('Title'); 
 function setTitle(x) {
     Title.textContent = x; 
 }
 
+//targeting the p 
 const Description = document.getElementById('Description'); 
 function setDescription(x) {
     Description.textContent = x; 
 }
 
+//targeting the entire body 
 const BackgroundColor = document.getElementById('BackgroundColor'); 
 function setBackgroundColor(x) {
     BackgroundColor.style.backgroundColor = x; 
 }
 
+//targeting the div that only contains text elements (h1/p)
 const FontColor = document.getElementById('color'); 
 function setFontColor(x) {
     FontColor.style.color = x; 
 }
 
-const light = document.querySelector('.light-mode')
-const dark = document.querySelector('.dark-mode')
+//targeting the 2 theme options
 
 function setTheme(x) {
-    if (x = "light-mode") {
-        BackgroundColor.classList.add('light-mode') 
+    if (x = "light") {
+        BackgroundColor.classList.add('light-mode')
+        BackgroundColor.classList.remove('dark-mode')
+    }else if (x = "dark") {
+        BackgroundColor.classList.add('dark-mode')
+        BackgroundColor.classList.remove('light-mode')
     }
-
-    else if (x = "dark-mode") {
-        BackgroundColor.classList.add = ('dark-mode') 
-    }
-
 }
 
+// for some reason, passing "dark" doesnt trigger the else if in the console 
 
-
-
-//replace text content 
-//document.getElementById('Title').innerHTML = 'Console Controller';
-//document.getElementById('Description').innerHTML = 'Filler text, description text, blah blah blah.';
-
-
-// make the light/dark mode button work
+//make the light/dark mode button work
 function myFunction() {
     const element = document.body;
     element.classList.toggle("dark-mode");
-  }
+}
